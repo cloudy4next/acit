72c4f23e8f090feafca48e5134595a737fee0d00
clouyd4next <jahangir7200@live.com>
2023-04-10 10:53:00 +0600

    fix

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -35,7 +35,7 @@ class ApiAuthController extends Controller
 
     public function login (Request $request) {
         $validator = Validator::make($request->all(), [
-            'loginCode' => 'required|string|min:8|max:8',
+            'loginCode' => 'required|string|min:6',
         ]);
         if ($validator->fails())
         {
