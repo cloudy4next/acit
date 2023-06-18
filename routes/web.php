dabc958c6c11c8ccbbb6fb8c74a91ad05050bb8f
jahangir <jahangir7200@live.com>
2023-06-19 00:19:57 +0600

    feat:privacy

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin/login');
 });
+
+Route::get('/privacy-policy', function () {
+    return view('home.privacy_policy');
+});
