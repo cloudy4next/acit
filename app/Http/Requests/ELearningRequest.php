8f1ec02da6f3db181dffd87fe7877d3b7afd69c4
jahangir <jahangir7200@live.com>
2023-06-19 21:27:11 +0600

    chore on Requesteleearning

--- app/Http/Requests/ELearningRequest.php
+++ app/Http/Requests/ELearningRequest.php
@@ -25,7 +25,9 @@ class ELearningRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'title' => 'required',
+            'title' => 'required'
+
         ];
     }
 
